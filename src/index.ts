789b70917ffea3ca60694f5762dd4d6f176541e8
Julien Avezou <julien.avezou@gmail.com>
2021-01-25 23:06:07 +0100

    setup graphql

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./entities/resolvers/hello";
+import { PostResolver } from "./entities/resolvers/post";
 
 
 const main = async () => {
@@ -8,6 +13,25 @@ const main = async () => {
   const orm = await MikroORM.init(microConfig);
   // run migration
   await orm.getMigrator().up();
+
+  // setup server
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ 
+      resolvers: [HelloResolver, PostResolver],
+      validate: false
+    }),
+    context: () => ({ em: orm.em })
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  })
+
+
 
   // em ==> entity manager
   // create new instance of Post (not saved yet!)
